9c41239071b5e76e8a5cd0c126504b99fc248332
Duong Cong Dinh <duongcongdinh.2006@gmail.com>
2026-04-12 09:26:25 +0700

    feat: setup initial clean project structure

---
+++ TrainingManagement.WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TrainingManagement.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
